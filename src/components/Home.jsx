7031ab8e0db67ffae4afac1a781bbe061bf593c7
Amitbalter <amitbaltern@gmail.com>
2025-02-17 17:35:12 +0000

    task-7

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,6 @@ export default function Home() {
 
     useEffect(() => {
         api.get("/articles").then((response) => {
-            console.log(response.data);
             setArticles(response.data);
         });
     }, []);
