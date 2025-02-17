0afce4df16cfd613e5c82bdf222357fc5d3eccad
Amitbalter <amitbaltern@gmail.com>
2025-02-17 17:12:48 +0000

    task-5

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
+import Article from "./components/Article";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="article/:id" element={<Article />} />
             </Routes>
         </BrowserRouter>
     );
